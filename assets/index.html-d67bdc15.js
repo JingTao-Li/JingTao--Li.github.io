f1e96f6a8fe383d853fb7f82b88550867ef7cb88
李京涛 <doyoudo_jingtao@163.com>
2023-02-22 20:10:25 +0800

    transform vuepress

---
+++ assets/index.html-d67bdc15.js
@@ -0,0 +1,16 @@
+import{_ as n,p as r,q as c,Q as e}from"./framework-5866ffd3.js";const t={};function l(d,o){return r(),c("div",null,[e(" # Hello VuePress "),e(` '/': [
+      {
+        text: 'Readme',
+        link: '/',
+      },
+      {
+        text: 'css技巧',
+        collapsible: true,
+        children: ['/reference/cli.md', '/reference/config.md'],
+      },
+      {
+        text: 'JS',
+        collapsible: true,
+        children: ['/reference/bundler/vite.md', '/reference/bundler/webpack.md'],
+      },
+    ], `)])}const i=n(t,[["render",l],["__file","index.html.vue"]]);export{i as default};
